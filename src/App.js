1719aa451f645233b362ab2085915275ad41320e
Rossita <47862499+AaronsonRossita@users.noreply.github.com>
2023-11-20 21:24:36 +0200

    First

--- src/App.js
+++ src/App.js
@@ -5,27 +5,57 @@ import { SongList } from './components/SongList';
 
 function App() {
 
+  console.log("app is rendered")
+
   const [songs, setSongs] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [artist, setArtist] = useState("");
+  const [isFetch, setIsFetch] = useState(true);
 
-  const API_KEY = "7ae4ff775c4512c4fa3cec05de128eda";
-  const myUrl = "https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&api_key=7ae4ff775c4512c4fa3cec05de128eda&artist=cher&format=json";
+  const LAST_FM_API_KEY = "7ae4ff775c4512c4fa3cec05de128eda";
+  const LAST_FM_METHOD_TOP_TRACKS = "artist.gettoptracks";
+  const LAST_FM_FORMAT_JSON = "json";
+  const LAST_FM_ARTIST = artist;
 
-  const getSongs = (url) => {
-    fetch(url)
-      .then( (response) => response.json())
+  const baseUrl = new URL("https://ws.audioscrobbler.com/2.0/");
+  const params = {
+    api_key: LAST_FM_API_KEY,
+    method: LAST_FM_METHOD_TOP_TRACKS,
+    format: LAST_FM_FORMAT_JSON,
+    artist: LAST_FM_ARTIST
+  }
+  
+  for (let key in params){
+    baseUrl.searchParams.append(key,params[key]);
+  }
+  
+  const fetchSongs = () => {
+    fetch(baseUrl)
+      .then( (res) => res.json())
       .then( (data) => setSongs(data.toptracks.track))
       .catch( (err) => console.log(err.message))
   }
 
   useEffect( () => {
-    getSongs(myUrl);
+    fetchSongs();
     setIsLoading(false);
-  }, [])
+    setArtist("");
+  },[isFetch])
+
+  const handleClick = () => {
+    setIsFetch(!isFetch);
+  }
+
+  const handleChange = (event) => {
+    setArtist(event.target.value)
+  }
 
   return (
     <div>
-      {isLoading ? <p>Loading...</p> : <SongList songs={songs}/>}
+      <input type='text' onChange={handleChange} value={artist}/>
+      <br></br>
+      <button onClick={handleClick}>Fetch</button>
+      {isLoading ? <p>Loading...</p> : songs.length == 0 ? <p>Type artist..</p> : <SongList songs={songs}/>}
     </div>
   );
 }
